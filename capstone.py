91a33dedf9b9105e9df363441cd113e008c2a577
harryim94 <77992524+harryim94@users.noreply.github.com>
2021-01-30 10:25:42 -0500

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### _This notebook will be maninly used for the_ 
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
